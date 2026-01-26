f49864ff012ad46c88ad516bc94b1e12f2f7f32e
faragon <miguelanga1604@gmail.com>
2026-01-26 02:36:23 -0500

    Feat: new mehotds

--- Gorgonix-Back.Domain/Entities/Movie.cs
+++ Gorgonix-Back.Domain/Entities/Movie.cs
@@ -2,5 +2,40 @@ namespace Gorgonix_Back.Domain.Entities;
 
 public class Movie
 {
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public string Genre { get; private set; } // TODO cambiar por enum 
+    public string PosterUrl { get; private set; }
+    public string PosterPublicId { get; private set; }
+    public string VideoUrl { get; private set; }
+    public string VideoPublicId { get; private set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
     
+    public ICollection<UserFavorite> FavoritedBy { get; private set; } = new List<UserFavorite>();
+
+    protected Movie() { }
+
+    public Movie(string title, string description, string genre, string posterUrl, string posterPublicId, string videoUrl, string videoPublicId)
+    {
+        if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Título requerido");
+        if (string.IsNullOrWhiteSpace(videoUrl)) throw new ArgumentException("Video requerido");
+
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+        Genre = genre;
+        PosterUrl = posterUrl;
+        PosterPublicId = posterPublicId;
+        VideoUrl = videoUrl;
+        VideoPublicId = videoPublicId;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void Update(string title, string description, string genre)
+    {
+        if (!string.IsNullOrWhiteSpace(title)) Title = title;
+        if (!string.IsNullOrWhiteSpace(description)) Description = description;
+        if (!string.IsNullOrWhiteSpace(genre)) Genre = genre;
+    }
 }
--- Gorgonix-Back.Domain/Interfaces/IMovieRepository.cs
+++ Gorgonix-Back.Domain/Interfaces/IMovieRepository.cs
@@ -1,6 +1,19 @@
+using Gorgonix_Back.Domain.Entities;
+
 namespace Gorgonix_Back.Domain.Interfaces;
 
-public class IMovieRepository
+public interface IMovieRepository
 {
+    Task<IEnumerable<Movie>> GetAllAsync();
+    Task<IEnumerable<Movie>> SearchAsync(string? name, string? genre);
+    Task<Movie?> GetByIdAsync(Guid id);
+    Task AddAsync(Movie movie);
+    Task UpdateAsync(Movie movie);
+    Task DeleteAsync(Movie movie);
     
+    // Métodos para favoritos
+    Task AddFavoriteAsync(UserFavorite favorite);
+    Task RemoveFavoriteAsync(Guid userId, Guid movieId);
+    Task<IEnumerable<Movie>> GetFavoritesByUserIdAsync(Guid userId);
+    Task<bool> IsFavoriteAsync(Guid userId, Guid movieId);
 }
